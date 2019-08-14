f63da42c33b17cd7ae4c589c4b6eefb42593bee5
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-14 12:14:43 +0500

    Базовый компонент Homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Homepage from "./Homepage";
 
 function App() {
   return (
     <div>
-      
+      <Homepage />
     </div>
   );
 }
