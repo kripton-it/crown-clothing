fa107f8098d48a442d01414a57006138cb08e93d
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-14 14:56:20 +0500

    Пример роутинга

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
-import "./App.css";
+import { Route, Switch } from "react-router-dom";
+
 import HomePage from "./pages/homepage/HomePage";
+
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/hats" component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
