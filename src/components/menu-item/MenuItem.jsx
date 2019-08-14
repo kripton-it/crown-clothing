0b97253904fd4a23030468708a88d2ba6b0aa404
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-14 14:26:46 +0500

    Оформление MenuItem

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -4,10 +4,11 @@ import "./MenuItem.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      className={`menu-item menu-item-${size}`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={`menu-item menu-item-${size}`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">Shop now</span>
