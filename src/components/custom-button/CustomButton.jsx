b4ff4e9e2f88d57c3039ebb85500a4d3aa131940
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-18 15:16:48 +0500

    Оформление кнопки добавления в корзину

--- src/components/custom-button/CustomButton.jsx
+++ src/components/custom-button/CustomButton.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import "./CustomButton.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...restProps }) => {
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...restProps }) => {
   return (
-    <button className={`custom-button${isGoogleSignIn ? " google-sign-in" : ""}`} {...restProps}>
+    <button
+      className={`custom-button${isGoogleSignIn ? " google-sign-in" : ""}${inverted ? " inverted" : ""}`}
+      {...restProps}
+    >
       {children}
     </button>
   );
