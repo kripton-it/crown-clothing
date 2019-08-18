8eb6882607af883cff11276e4fcd2885ff1375d1
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-18 23:05:55 +0500

    Компонент СheckoutItem

--- src/pages/checkout-page/CheckoutPage.jsx
+++ src/pages/checkout-page/CheckoutPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
+import CheckoutItem from "../../components/checkout-item/CheckoutItem";
+
 import {
   selectCartItems,
   selectCartTotalPrice
@@ -29,8 +31,12 @@ const CheckoutPage = ({ cartItems, total }) => {
           <span>Remove</span>
         </div>
       </div>
-      {cartItems.map(cartItem => cartItem.name)}
-      <div className="total"><span>Total: ${total}</span></div>
+      {cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className="total">
+        <span>Total: ${total}</span>
+      </div>
     </div>
   );
 };
