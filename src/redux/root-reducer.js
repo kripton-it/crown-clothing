8e08d7471e4bcfb33479a1bd0cc9874c7b898498
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-19 10:31:00 +0500

    Реализация хранения данных между сессиями с помощью redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+// для localStorage
+import storage from "redux-persist/lib/storage";
+// для sessionStorage
+// import sessionStorage from 'redux-persist/lib/storage/session'
 
 import userReducer from "./user/user-reducer";
 import cartReducer from "./cart/cart-reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whiteList: ["cart"]
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
-})
+});
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
@@ -6,6 +7,8 @@ import rootReducer from "./root-reducer";
 // const middlewares = [logger]
 
 //const store = createStore(rootReducer, applyMiddleware(...middlewares))
-const store = createStore(rootReducer, applyMiddleware(logger));
+export const store = createStore(rootReducer, applyMiddleware(logger));
 
-export default store;
+export const persistor = persistStore(store);
+
+// export default { store, persistor };
