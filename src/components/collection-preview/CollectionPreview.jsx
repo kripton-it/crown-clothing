cdd3de8791d752382c11c4143f19e5a201984674
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-21 22:41:26 +0500

    Рефакторинг стилей компонента CollectionPreview

--- src/components/collection-preview/CollectionPreview.jsx
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -1,24 +1,33 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import CollectionItem from "../collection-item/CollectionItem";
 
-import "./CollectionPreview.styles.scss";
+import {
+  CollectionPreviewContainer,
+  CollectionTitle,
+  CollectionList
+} from "./CollectionPreviewStyles";
 
 const PREVIEWS_NUMBER = 4;
 
-const CollectionPreview = ({ collection }) => {
+const CollectionPreview = ({ collection, history, match, routName }) => {
   const { title, items } = collection;
 
   return (
-    <div className="collection-preview">
-      <h1 className="title">{title}</h1>
-      <div className="preview">
+    <CollectionPreviewContainer>
+      <CollectionTitle
+        onClick={() => history.push(`${match.path}/${routName}`)}
+      >
+        {title}
+      </CollectionTitle>
+      <CollectionList>
         {items.slice(0, PREVIEWS_NUMBER).map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </CollectionList>
+    </CollectionPreviewContainer>
   );
 };
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
