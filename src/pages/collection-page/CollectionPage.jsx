8903b3e84997e20a97b03f4027461db55a872760
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-24 00:06:04 +0500

    Рефакторинг стилей компонента CollectionPage

--- src/pages/collection-page/CollectionPage.jsx
+++ src/pages/collection-page/CollectionPage.jsx
@@ -5,25 +5,33 @@ import { selectCollection } from "../../redux/shop/shop-selectors";
 
 import CollectionItem from "../../components/collection-item/CollectionItem";
 
-import "./CollectionPage.styles.scss";
+import {
+  CollectionPageContainer,
+  TitleContainer,
+  ItemsContainer
+} from "./CollectionPageStyles";
 
 const CollectionPage = ({ collection }) => {
-  const {title, items} = collection;
+  const { title, items } = collection;
 
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionPageContainer>
+      <TitleContainer>{title}</TitleContainer>
+      <ItemsContainer>
         {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
+          <CollectionItem
+            className="collection-item"
+            key={item.id}
+            item={item}
+          />
         ))}
-      </div>
-    </div>
+      </ItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state)
-})
+});
 
 export default connect(mapStateToProps)(CollectionPage);
